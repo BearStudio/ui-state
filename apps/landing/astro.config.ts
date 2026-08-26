0d8dc097383895627e68fe174c9975b34edb49eb
Hugo Pérard <hugo.perard@bearstudio.fr>
2026-08-26 13:58:40 +0200

    update astro

--- apps/landing/astro.config.ts
+++ apps/landing/astro.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, fontProviders } from "astro/config";
 
 export default defineConfig({
   output: "static",
+  compressHTML: true,
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
